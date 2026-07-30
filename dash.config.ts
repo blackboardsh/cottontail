0c178b6a8d2cb900133de2d2f7397344158f4f7b
Yoav <hey@yoav.codes>
2026-07-30 19:32:30 -0400

    v0.2.0-canary.11

--- dash.config.ts
+++ dash.config.ts
@@ -1,4 +1,4 @@
-// @dash cli=0.2.1 cottontail=0.2.0-canary.10
+// @dash cli=0.2.1 cottontail=0.2.0-canary.11
 export default {
   scripts: {
     "push:canary": "node scripts/tag-release.js canary",
