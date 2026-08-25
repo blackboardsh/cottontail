313698aa7c6640755da1ef53ceceeae27dd20c37
Yoav <hey@yoav.codes>
2026-08-25 18:48:35 -0400

    v0.6.0-canary.8

--- hutch.config.ts
+++ hutch.config.ts
@@ -1,4 +1,4 @@
-// @hutch cli=0.5.1 cottontail=0.6.0-canary.7
+// @hutch cli=0.5.1 cottontail=0.6.0-canary.8
 export default {
   scripts: {
     "push:canary": "node scripts/tag-release.js canary",
--- src/version.zig
+++ src/version.zig
@@ -1,1 +1,1 @@
-pub const version = "0.6.0-canary.7";
+pub const version = "0.6.0-canary.8";
