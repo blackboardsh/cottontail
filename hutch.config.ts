b1130364d5cc70882241753cf6c1ad7a9db730a4
Yoav Givati <hey@yoav.codes>
2026-08-29 16:05:30 -0700

    v0.6.0-canary.13

--- hutch.config.ts
+++ hutch.config.ts
@@ -1,4 +1,4 @@
-// @hutch cli=0.5.1 cottontail=0.6.0-canary.12
+// @hutch cli=0.5.1 cottontail=0.6.0-canary.13
 export default {
   scripts: {
     "push:canary": "node scripts/tag-release.js canary",
--- src/version.zig
+++ src/version.zig
@@ -1,1 +1,1 @@
-pub const version = "0.6.0-canary.12";
+pub const version = "0.6.0-canary.13";
